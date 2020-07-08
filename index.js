9346d2abc3f7d4b9b4861c7a8dfe0a5b053a15b5
Sanjeev Yogi <yogi.sjv@gmail.com>
2020-07-08 22:16:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ function takeANumber(line, name) {
 }
 
 
-// function nowServing(line) {
-//   if (!line.length) {
-//     return  "There is nobody waiting to be served!"
-//   } else {
-//     console.log(line.shift())
-//   }
-// }
+function nowServing(line) {
+  if (!line.length) {
+    return  "There is nobody waiting to be served!"
+  } else {
+    console.log(line.shift())
+  }
+}
 
-// function currentLine(line) {
-//   array = []
-//   for (let i = 0; i < line.length; i++) {
-//   if (!line.length) {
-//     "The line is currently empty."
-//   } else {
-//     array.push(`${parseInt(i) + 1}. ${line[i]}`)
-//   }
-// }
-// return `The line is currently: ${array.join(", ")}`
-// }
+function currentLine(line) {
+  array = []
+  for (let i = 0; i < line.length; i++) {
+  if (!line.length) {
+    "The line is currently empty."
+  } else {
+    array.push(`${parseInt(i) + 1}. ${line[i]}`)
+  }
+}
+return `The line is currently: ${array.join(", ")}`
+}
