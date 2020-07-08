d970082a2321b37bee28589b8a1cf359ee675153
Sanjeev Yogi <yogi.sjv@gmail.com>
2020-07-08 22:15:53 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function takeANumber(line, name) {
+  line.push(name)
+  console.log(`Welcome, ${name}. You are ${line.length} in line.`)
+}
+
+
+// function nowServing(line) {
+//   if (!line.length) {
+//     return  "There is nobody waiting to be served!"
+//   } else {
+//     console.log(line.shift())
+//   }
+// }
+
+// function currentLine(line) {
+//   array = []
+//   for (let i = 0; i < line.length; i++) {
+//   if (!line.length) {
+//     "The line is currently empty."
+//   } else {
+//     array.push(`${parseInt(i) + 1}. ${line[i]}`)
+//   }
+// }
+// return `The line is currently: ${array.join(", ")}`
+// }
